6b49e796baedffc26854a45b6ea1cb74179081c7
Winston White <whitew2015@fau.edu>
2025-07-16 17:39:09 -0400

    testing audio with button clicks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+const audio = new Audio(`https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3`);
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        audio.play();
+      }}>Hello World</button>
+    
+      {/*<button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>
+      <button>Hello World</button>*/}
     </div>
   );
 }
